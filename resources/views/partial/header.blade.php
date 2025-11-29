2a33adb8cfe07190a5a4a79c3148563ec425634d
rafi1467 <rafi22205101467@diu.edu.bd>
2025-11-29 13:42:32 +0600

    ok

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar Example</title>
+    <title>My Portfolio</title>
     <style>
         /* Navbar container */
         .navbar-flex {
